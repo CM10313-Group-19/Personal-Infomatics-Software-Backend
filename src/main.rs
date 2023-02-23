3530a08a99bc9e41e4bd4a7785319e2f2b1ecbec
Harry Crane <37228043+hwcrane@users.noreply.github.com>
2023-02-23 15:48:13 +0000

    Commented code and moved meal code into meal.rs

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,19 @@
 #[macro_use]
 extern crate rocket;
+use rocket_db_pools::Database;
 
-use chrono::NaiveDateTime;
-use rocket::serde::json::Json;
-use rocket_db_pools::{Connection, Database};
-use serde::{Deserialize, Serialize};
+mod meal;
+
+use crate::meal::{get_meals, new_meal};
 
 // Define the databse
 #[derive(Database)]
 #[database("test")]
-struct Meals(sqlx::MySqlPool);
-
-#[derive(Serialize, Deserialize)]
-struct NewMeal {
-    meal_name: String,
-    date_eaten: NaiveDateTime,
-    calories: i32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Meal {
-    meal_id: i32,
-    meal_name: String,
-    date_eaten: NaiveDateTime,
-    calories: i32,
-}
-
-#[get("/meal")]
-async fn get_meals(mut db: Connection<Meals>) -> Option<Json<Vec<Meal>>> {
-    let r = sqlx::query_as!(Meal, "SELECT * FROM meals")
-        .fetch_all(&mut *db)
-        .await
-        .ok()?;
-    Some(Json(r))
-}
-
-#[post("/meal", data = "<meal_data>")]
-async fn new_meal(meal_data: Json<NewMeal>, mut db: Connection<Meals>) {
-    sqlx::query!(
-        "INSERT INTO meals (meal_name, date_eaten, calories)
-         VALUES (?, ?, ?)",
-        meal_data.meal_name,
-        meal_data.date_eaten,
-        meal_data.calories
-    )
-    .execute(&mut *db)
-    .await
-    .ok();
-}
+pub struct Db(sqlx::MySqlPool);
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .attach(Meals::init())
+        .attach(Db::init())
         .mount("/", routes![get_meals, new_meal])
 }
---
+++ src/meal.rs
@@ -0,0 +1,61 @@
+use chrono::NaiveDateTime;
+use rocket::serde::json::Json;
+use rocket_db_pools::Connection;
+use serde::{Deserialize, Serialize};
+use crate::Db;
+
+// Struct for recieving a new meal
+// Does not need meal_id item as id auto_incremented
+#[derive(Serialize, Deserialize)]
+pub struct NewMeal {
+    meal_name: String,
+    date_eaten: NaiveDateTime,
+    calories: i32,
+}
+
+// Struct for a meal entry
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Meal {
+    meal_id: i32,
+    meal_name: String,
+    date_eaten: NaiveDateTime,
+    calories: i32,
+}
+
+/// Fetches all the meals in the database
+#[get("/meal")]
+pub async fn get_meals(mut db: Connection<Db>) -> Option<Json<Vec<Meal>>> {
+    let result = 
+        // Construct the SQL query, indicate the date returned should be in Meal structs
+        sqlx::query_as!(Meal, "SELECT * FROM meals")
+        // execute the query
+        .fetch_all(&mut *db)
+        // fetch_all is async so wait to be complete
+        .await
+        // Convert result into option
+        .ok()?;
+
+    // Return the meals in json format
+    Some(Json(result))
+}
+
+/// Inserts a new meal into the database
+#[post("/meal", data = "<meal_data>")]
+pub async fn new_meal(meal_data: Json<NewMeal>, mut db: Connection<Db>) {
+    // Construct the query
+    sqlx::query!(
+        "INSERT INTO meals (meal_name, date_eaten, calories)
+         VALUES (?, ?, ?)",
+        meal_data.meal_name,
+        meal_data.date_eaten,
+        meal_data.calories
+    )
+    // execute the query
+    .execute(&mut *db)
+    // fetch_all is async so wait to be complete
+    .await
+    // Convert result into option
+    .ok();
+
+    // TODO: return status of the insert
+}
