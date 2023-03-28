167b5bcc8110339ec49f466005ed18c241e4a856
Harry Crane <37228043+hwcrane@users.noreply.github.com>
2023-03-28 17:07:22 +0100

    added meals to be stored and retrived

--- src/meal.rs
+++ src/meal.rs
@@ -1,14 +1,6 @@
-use crate::*;
+use rocket::form::Form;
 
-// Struct for recieving a new meal
-// Does not need meal_id item as id auto_incremented
-#[derive(Serialize, Deserialize)]
-pub struct NewMeal {
-    user_id: i32,
-    meal_name: String,
-    date_eaten: NaiveDateTime,
-    calories: i32,
-}
+use crate::*;
 
 // Struct for a meal entry
 #[derive(Debug, Serialize, Deserialize)]
@@ -19,27 +11,36 @@ pub struct Meal {
     calories: i32,
 }
 
-// Fetches all the meals in the database
-#[get("/meal")]
-pub async fn get_meals(mut db: Connection<Db>) -> Option<Json<Vec<Meal>>> {
+// Fetches all the meals in the database for a given user
+#[get("/meal?<id>")]
+pub async fn get_meals(id: i32, mut db: Connection<Db>) -> Option<Json<Vec<Meal>>> {
     let result =
         // Construct the SQL query, indicate the date returned should be in Meal structs
-        sqlx::query_as!(Meal, "SELECT meal_id, meal_name, date_eaten, calories FROM meals")
+        sqlx::query_as!(Meal, "SELECT meal_id, meal_name, date_eaten, calories FROM meals WHERE user_id = ?", id)
         // execute the query
         .fetch_all(&mut *db)
         // fetch_all is async so wait to be complete
         .await
         // Convert result into option
         .ok()?;
-    println!("{:?}", result);
 
     // Return the meals in json format
     Some(Json(result))
 }
 
+// Struct for recieving a new meal
+// Does not need meal_id item as id auto_incremented
+#[derive(FromForm)]
+pub struct NewMeal {
+    user_id: i32,
+    meal_name: String,
+    date_eaten: String,
+    calories: i32,
+}
+
 // Inserts a new meal into the database
 #[post("/meal", data = "<meal_data>")]
-pub async fn new_meal(meal_data: Json<NewMeal>, mut db: Connection<Db>) {
+pub async fn new_meal(meal_data: Form<NewMeal>, mut db: Connection<Db>) {
     // Construct the query
     let result = sqlx::query!(
         "INSERT INTO meals (user_id, meal_name, date_eaten, calories)
